4d35800e71fb015c9e17fd3835563bef0462506e
icey <iceyokolo@gmail.com>
2022-06-23 15:57:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+user = prompt("What is your name?: ");
+userHeight = prompt("What is your height?: ");
+userCountry = prompt("What is your country?: ");
+
+console.log(
+  `Your name is ${user}, you are ${userHeight}cm tall and you are from ${userCountry}.`
+);
+
+alert(
+  `Your name is ${user}, you are ${userHeight}cm tall and you are from ${userCountry}.`
+);
