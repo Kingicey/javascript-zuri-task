ad04ce8f84e8209e87debe61600efef888c6996c
Okolo Ifeanyi Christian <iceyokolo@gmail.com>
2022-06-25 16:35:48 +0100

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-user = prompt("What is your name?: ");
-userHeight = prompt("What is your height?: ");
-userCountry = prompt("What is your country?: ");
+let user = prompt("What is your name?: ");
+let userHeight = prompt("What is your height?: ");
+let userCountry = prompt("What is your country?: ");
 
 console.log(
   `Your name is ${user}, you are ${userHeight}cm tall and you are from ${userCountry}.`
